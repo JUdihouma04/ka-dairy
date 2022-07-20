954ddd273527398ebaa0de8f8b456fd55581b786
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-07-20 09:47:47 +0300

    add the assets and the files

---
+++ script.js
@@ -0,0 +1,16 @@
+var totalProduction = function () {};
+let login = document.querySelector("button");
+login.onclick = function () {
+	button.classlist.toggle("active");
+};
+var farm = {
+    sheds["A", "B", "C","D"]
+    Production[123, 234, 423, 126]
+};
+
+var totalproduction = function () {
+    var total = 0;
+    for(var 1 = 0; 1 < farm.sheds.length; 1++){
+        console.log(  "production for shed " = farm.sheds[1] + "15" + farm.production [1] + litres")
+    }
+}
